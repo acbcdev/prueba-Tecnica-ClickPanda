5b5bc24e86b2dce20ccdabdae41072e7469f7781
acbc <acbc.dev@gmail.com>
2025-04-02 13:15:14 -0500

    refactor Error component: simplify error handling UI and improve layout

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,33 +1,18 @@
 "use client";
 
 import { Button } from "@heroui/button";
-import { useEffect } from "react";
 
 // biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
 export default function Error({
-	error,
 	reset,
 }: {
-	error: Error;
 	reset: () => void;
 }) {
-	useEffect(() => {
-		// Log the error to an error reporting service
-		/* eslint-disable no-console */
-		console.error(error);
-	}, [error]);
-
 	return (
-		<div>
-			<h2>Something went wrong!</h2>
-			<Button
-				onClick={
-					// Attempt to recover by trying to re-render the segment
-					() => reset()
-				}
-			>
-				Try again
-			</Button>
+		<div className="absolute inset-0 grid gap-2 place-content-center">
+			<h2 className="text-xl">Something went wrong! </h2>
+
+			<Button onClick={() => reset()}>Try again</Button>
 		</div>
 	);
 }

abe5c5f9efc30a6212013cc10ec82c8a3badc480
acbc <acbc.dev@gmail.com>
2025-04-02 10:43:41 -0500

    initialize Next.js & HeroUI template with essential configuration files and assets

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+import { card, heroui } from "@heroui/theme";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+      colors: {
+        border: "var(--border)",
+        card: "var(--card)",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [
+    heroui({
+      themes: {
+        light: {
+          colors: {
+            // background: "#e8f4fc",
+            divider: "#c3c3c3",
+            foreground: "#001042",
+            content1: "#ffffff",
+            secondary: {
+              DEFAULT: "#ff7f22",
+              secondary: "#ff7f22",
+              500: "#fea259",
+            },
+            primary: {
+              500: "#1d77c0",
+              DEFAULT: "#4a77ba",
+            },
+          },
+        },
+        dark: {
+          colors: {
+            background: "#111521",
+            primary: {
+              DEFAULT: "#1d77c0",
+              500: "#4a77ba",
+            },
+            secondary: {
+              DEFAULT: "#ff7f22",
+              secondary: "#ff7f22",
+              500: "#fea259",
+            },
+            content1: "#1d212c",
+            foreground: "#e8f4fc",
+          },
+        },
+      },
+    }),
+  ],
+};
+
+module.exports = config;

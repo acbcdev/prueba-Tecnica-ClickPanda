d5af0f23b9e900f0bb90e9c90ac42626c8104902
acbc <acbc.dev@gmail.com>
2025-04-02 10:44:13 -0500

    add configuration files for endpoints, fonts, and site settings

---
+++ src/config/endpoints.ts
@@ -0,0 +1,22 @@
+type PropsFlags = {
+	countryCode: string;
+	style?: "flat" | "shiny";
+	size?: "16" | "24" | "32" | "48" | "64";
+};
+
+const BASE_URL = "https://restcountries.com/v3.1";
+
+export const endpoints = {
+	countries: {
+		base: "https://restcountries.com",
+		all: `${BASE_URL}/all`,
+		name: (name: string) => `${BASE_URL}/name/${name}`,
+		region: (region: string) => `${BASE_URL}/region/${region}`,
+		currency: (currency: string) => `${BASE_URL}/currency/${currency}`,
+		lang: (lang: string) => `${BASE_URL}/lang/${lang}`,
+		subregion: (subregion: string) => `
+${BASE_URL}/subregion/${subregion}`,
+	},
+	flags: ({ countryCode, style = "flat", size = "32" }: PropsFlags) =>
+		`https://flagsapi.com/${countryCode}/${style}/${size}.png`,
+};

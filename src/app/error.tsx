5dab7191f6fdf43ef1c7615ed53356ba1865fc2d
acbc <acbc.dev@gmail.com>
2025-04-02 10:45:41 -0500

    add error handling component with retry functionality

---
+++ src/app/error.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@heroui/button";
+import { useEffect } from "react";
+
+// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+export default function Error({
+	error,
+	reset,
+}: {
+	error: Error;
+	reset: () => void;
+}) {
+	useEffect(() => {
+		// Log the error to an error reporting service
+		/* eslint-disable no-console */
+		console.error(error);
+	}, [error]);
+
+	return (
+		<div>
+			<h2>Something went wrong!</h2>
+			<Button
+				onClick={
+					// Attempt to recover by trying to re-render the segment
+					() => reset()
+				}
+			>
+				Try again
+			</Button>
+		</div>
+	);
+}

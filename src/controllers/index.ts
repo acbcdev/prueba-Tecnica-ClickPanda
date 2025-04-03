2ac7cbb87301e7797b978f74c94f88d116b0895d
acbc <acbc.dev@gmail.com>
2025-04-02 20:52:16 -0500

    feat(countries): update API endpoints for country data retrieval and add getCountryByCode function

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,17 @@
-import { endpoints } from "@/config/endpoints";
 import type { Country } from "@/types";
+
 import axios from "axios";
 
+import { endpoints } from "@/config/endpoints";
+
 export async function getAllCountries() {
-	const response = await axios.get<Country[]>(endpoints.countries.all);
+	//
+	// const response = await axios.get<Country[]>(endpoints.countries.all);
+	// return response.data;
+	const response = await axios.get<Country[]>(
+		"http://localhost:3000/restcountriescomall.json",
+	);
+
 	return response.data;
 }
 
@@ -11,12 +19,26 @@ export async function getCountryByName(name: string) {
 	const response = await axios.get<Country[]>(
 		endpoints.countries.name(name.trim()),
 	);
+
 	return response.data;
+}
+
+export async function getCountryByCode(code: string) {
+	try {
+		const response = await axios.get<Country[]>(
+			endpoints.countries.code(code.trim()),
+		);
+
+		return response.data[0];
+	} catch (error) {
+		return null;
+	}
 }
 
 export async function getCountryByRegion(region: string) {
 	const response = await axios.get<Country[]>(
 		endpoints.countries.region(region.trim()),
 	);
+
 	return response.data;
 }

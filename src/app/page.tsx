72f77ffeec91abe67e930734274fc839cd5ab6bb
acbc <acbc.dev@gmail.com>
2025-04-03 00:08:03 -0500

    feat(page): wrap Dashboard component in Suspense for improved loading handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import { Dashboard } from "@/components/dashbord";
 import { getAllCountries } from "@/controllers";
+import { Suspense } from "react";
 
 export default async function Home() {
 	const res = await getAllCountries();
 
-	return <Dashboard countries={res} />;
+	return (
+		<Suspense>
+			<Dashboard countries={res} />
+		</Suspense>
+	);
 }

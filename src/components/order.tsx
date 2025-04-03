8e4c76d5937dac78f4f26e9602f487d2b7c87fa7
acbc <acbc.dev@gmail.com>
2025-04-03 00:08:53 -0500

    refactor(order): improve formatting and readability of Order component

--- src/components/order.tsx
+++ src/components/order.tsx
@@ -1,35 +1,37 @@
-import { SORT } from "@/config/filters";
-import { Select, SelectItem } from "@heroui/select";
 import type { Options } from "nuqs";
+
+import { Select, SelectItem } from "@heroui/select";
+
+import { SORT } from "@/config/filters";
 type OrderProps = {
-	sort: string;
-	setSort: (
-		value:
-			| "area"
-			| "name"
-			| "population"
-			| ((
-					old: "area" | "name" | "population",
-			  ) => "area" | "name" | "population" | null)
-			| null,
-		options?: Options,
-	) => Promise<URLSearchParams>;
+  sort: string;
+  setSort: (
+    value:
+      | "area"
+      | "name"
+      | "population"
+      | ((
+          old: "area" | "name" | "population",
+        ) => "area" | "name" | "population" | null)
+      | null,
+    options?: Options,
+  ) => Promise<URLSearchParams>;
 };
 export function Order({ sort, setSort }: OrderProps) {
-	return (
-		<Select
-			className="w-40"
-			defaultSelectedKeys={[sort]}
-			label="sort by"
-			labelPlacement="outside"
-			variant="bordered"
-			onChange={(e) => {
-				setSort(e.target.value as (typeof SORT)[number]);
-			}}
-		>
-			{SORT.map((sort) => (
-				<SelectItem key={sort}>{sort}</SelectItem>
-			))}
-		</Select>
-	);
+  return (
+    <Select
+      className="w-40"
+      defaultSelectedKeys={[sort]}
+      label="sort by"
+      labelPlacement="outside"
+      variant="bordered"
+      onChange={(e) => {
+        setSort(e.target.value as (typeof SORT)[number]);
+      }}
+    >
+      {SORT.map((sort) => (
+        <SelectItem key={sort}>{sort}</SelectItem>
+      ))}
+    </Select>
+  );
 }

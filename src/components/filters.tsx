c74222bd2205e6de9173330506a607b1abf9d762
acbc <acbc.dev@gmail.com>
2025-04-03 00:08:42 -0500

    refactor(filters): improve formatting and readability of Filters component

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,99 +1,98 @@
-import { REGIONS, SUB_REGIONS } from "@/config/filters";
 import { Button } from "@heroui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "@heroui/popover";
 import { Select, SelectItem } from "@heroui/select";
 import { ListFilter } from "lucide-react";
 
+import { REGIONS, SUB_REGIONS } from "@/config/filters";
+
 type PropsFilters = {
-	region: string;
-	setRegion: (value: string) => void;
-	subRegion: string;
-	setSubRegion: (value: string) => void;
-	lang: string;
-	setLang: (value: string) => void;
-	currency: string;
-	setCurrency: (value: string) => void;
-	langs: string[];
-	currencies: string[];
+  region: string;
+  setRegion: (value: string) => void;
+  setSubRegion: (value: string) => void;
+  lang: string;
+  setLang: (value: string) => void;
+  currency: string;
+  setCurrency: (value: string) => void;
+  langs: string[];
+  currencies: string[];
 };
 
 export function Filters({
-	region,
-	setRegion,
-	subRegion,
-	setSubRegion,
-	lang,
-	setLang,
-	currency,
-	setCurrency,
-	langs,
-	currencies,
+  region,
+  setRegion,
+  setSubRegion,
+  lang,
+  setLang,
+  currency,
+  setCurrency,
+  langs,
+  currencies,
 }: PropsFilters) {
-	return (
-		<Popover>
-			<PopoverTrigger>
-				<Button color="primary">
-					Filters <ListFilter />
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent className="grid grid-cols-2 gap-4 p-4">
-				<Select
-					className="w-44"
-					defaultSelectedKeys={[region]}
-					label="region"
-					onChange={(e) => {
-						setRegion(e.target.value);
-					}}
-				>
-					{REGIONS.map((region) => (
-						<SelectItem key={region}>{region}</SelectItem>
-					))}
-				</Select>
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button color="primary">
+          Filters <ListFilter />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="grid grid-cols-2 gap-4 p-4">
+        <Select
+          className="w-44"
+          defaultSelectedKeys={[region]}
+          label="region"
+          onChange={(e) => {
+            setRegion(e.target.value);
+          }}
+        >
+          {REGIONS.map((region) => (
+            <SelectItem key={region}>{region}</SelectItem>
+          ))}
+        </Select>
 
-				<Select
-					className="w-44 "
-					label="subRegion"
-					onChange={(e) => {
-						setSubRegion(e.target.value);
-					}}
-				>
-					{SUB_REGIONS.map((subregion) => (
-						<SelectItem key={subregion} title={subregion}>
-							{subregion}
-						</SelectItem>
-					))}
-				</Select>
+        <Select
+          className="w-44 "
+          label="subRegion"
+          onChange={(e) => {
+            setSubRegion(e.target.value);
+          }}
+        >
+          {SUB_REGIONS.map((subregion) => (
+            <SelectItem key={subregion} title={subregion}>
+              {subregion}
+            </SelectItem>
+          ))}
+        </Select>
 
-				<Select
-					className="w-44"
-					defaultSelectedKeys={[lang]}
-					label="language"
-					onChange={(e) => {
-						setLang(e.target.value);
-					}}
-				>
-					{langs.map((lang) => (
-						<SelectItem key={lang} title={lang}>
-							{lang}
-						</SelectItem>
-					))}
-				</Select>
+        <Select
+          className="w-44"
+          defaultSelectedKeys={[lang]}
+          label="language"
+          onChange={(e) => {
+            setLang(e.target.value);
+          }}
+        >
+          {langs.map((lang) => (
+            <SelectItem key={lang} title={lang}>
+              {lang}
+            </SelectItem>
+          ))}
+        </Select>
 
-				<Select
-					className="w-44"
-					defaultSelectedKeys={[currency]}
-					label="currency"
-					onChange={(e) => {
-						setCurrency(e.target.value);
-					}}
-				>
-					{currencies.map((currency) => (
-						<SelectItem key={currency} title={currency}>
-							{currency}
-						</SelectItem>
-					))}
-				</Select>
-			</PopoverContent>
-		</Popover>
-	);
+        <Select
+          className="w-44"
+          defaultSelectedKeys={[currency]}
+          label="currency"
+          onChange={(e) => {
+            setCurrency(e.target.value);
+          }}
+        >
+          {currencies.map((currency) => (
+            <SelectItem key={currency} title={currency}>
+              {currency}
+            </SelectItem>
+          ))}
+        </Select>
+      </PopoverContent>
+    </Popover>
+  );
 }

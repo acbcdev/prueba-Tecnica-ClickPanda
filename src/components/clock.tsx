ace74bd61b05d46422fa1f06300ccf52efcdd72e
acbc <acbc.dev@gmail.com>
2025-04-03 00:08:28 -0500

    refactor(clock): improve formatting and readability of UtcClock component

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -1,18 +1,19 @@
 "use client";
-import { getTimeWithOffset, parseUtcOffset } from "@/utils/lib";
 import { useEffect, useState } from "react";
 
+import { getTimeWithOffset, parseUtcOffset } from "@/utils/lib";
+
 export default function UtcClock({ offsetStr }: { offsetStr: string }) {
-	const offset = parseUtcOffset(offsetStr);
-	const [time, setTime] = useState(getTimeWithOffset(offset));
+  const offset = parseUtcOffset(offsetStr);
+  const [time, setTime] = useState(getTimeWithOffset(offset));
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setTime(getTimeWithOffset(offset));
-		}, 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(getTimeWithOffset(offset));
+    }, 1000);
 
-		return () => clearInterval(interval);
-	}, [offset]);
+    return () => clearInterval(interval);
+  }, [offset]);
 
-	return <div className="font-mono text-2xl">{time}</div>;
+  return <div className="font-mono text-2xl">{time}</div>;
 }

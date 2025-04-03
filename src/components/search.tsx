55f232f6077b0e9f8df4f6cc97d9cbcee794640c
acbc <acbc.dev@gmail.com>
2025-04-03 00:08:58 -0500

    refactor(search): improve formatting and readability of Search component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -4,34 +4,35 @@ import { useHotkeys } from "react-hotkeys-hook";
 import { SearchIcon } from "lucide-react";
 import { useRef } from "react";
 export function Search({
-	search,
-	setSearch,
+  search,
+  setSearch,
 }: {
-	search: string;
-	setSearch: React.Dispatch<React.SetStateAction<string>>;
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
 }) {
-	const searchRef = useRef<HTMLInputElement>(null);
-	useHotkeys(
-		"cmd+k,ctrl+k",
-		() => {
-			searchRef.current?.focus();
-		},
-		{ preventDefault: true },
-	);
+  const searchRef = useRef<HTMLInputElement>(null);
 
-	return (
-		<Input
-			className="max-w-xl"
-			ref={searchRef}
-			label="Search countries"
-			labelPlacement="outside"
-			startContent={<SearchIcon />}
-			value={search}
-			endContent={<Kbd keys={["command"]}>k</Kbd>}
-			variant="bordered"
-			onChange={(e) => {
-				setSearch(e.target.value);
-			}}
-		/>
-	);
+  useHotkeys(
+    "cmd+k,ctrl+k",
+    () => {
+      searchRef.current?.focus();
+    },
+    { preventDefault: true },
+  );
+
+  return (
+    <Input
+      ref={searchRef}
+      className="max-w-xl"
+      endContent={<Kbd keys={["command"]}>k</Kbd>}
+      label="Search countries"
+      labelPlacement="outside"
+      startContent={<SearchIcon />}
+      value={search}
+      variant="bordered"
+      onChange={(e) => {
+        setSearch(e.target.value);
+      }}
+    />
+  );
 }

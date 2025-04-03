67a6475fd382b526b2a4397b79882a5d64fd5ba6
acbc <acbc.dev@gmail.com>
2025-04-03 00:08:48 -0500

    refactor(list): improve formatting and readability of List component

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,30 +1,34 @@
 import type { Country } from "@/types";
-import { Card } from "@/components/card";
+
 import { Frown } from "lucide-react";
 
+import { Card } from "@/components/card";
+
 export function List({
-	paginatedCountries,
-}: { paginatedCountries: Country[] }) {
-	return (
-		<div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-			{paginatedCountries.length === 0 ? (
-				<div className="flex items-center justify-center my-4 gap-x-2">
-					<h2 className="text-xl ">No results found</h2>
-					<Frown />
-				</div>
-			) : (
-				paginatedCountries.map((country) => (
-					<Card
-						key={country.cca2}
-						currency={country.currencies}
-						flag={country.cca2}
-						population={country.population.toLocaleString()}
-						region={country.region}
-						title={country.name.common}
-						capital={country.capital?.join(", ") ?? ""}
-					/>
-				))
-			)}
-		</div>
-	);
+  paginatedCountries,
+}: {
+  paginatedCountries: Country[];
+}) {
+  return (
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {paginatedCountries.length === 0 ? (
+        <div className="flex items-center justify-center my-4 gap-x-2">
+          <h2 className="text-xl ">No results found</h2>
+          <Frown />
+        </div>
+      ) : (
+        paginatedCountries.map((country) => (
+          <Card
+            key={country.cca2}
+            capital={country.capital?.join(", ") ?? ""}
+            currency={country.currencies}
+            flag={country.cca2}
+            population={country.population.toLocaleString()}
+            region={country.region}
+            title={country.name.common}
+          />
+        ))
+      )}
+    </div>
+  );
 }
